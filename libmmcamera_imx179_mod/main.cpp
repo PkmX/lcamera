6fa49977b3e2e08612b1b13a35bdb00f7476cc9e
PkmX <pkmx.tw@gmail.com>
2015-02-06 22:04:45 +0800

    Make lib patcher work with latest boost.endian.

--- libmmcamera_imx179_mod/main.cpp
+++ libmmcamera_imx179_mod/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
-#include <boost/endian/types.hpp>
+#include <boost/endian/arithmetic.hpp>
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/positional_options.hpp>
@@ -58,23 +58,32 @@ auto cond_set_field(Output& dst, const po::variables_map& vm, const std::string&
 
 auto main(const int argc, const char * const * const argv) -> int
 {
-    po::options_description desc("Options");
-    desc.add_options()("active-pixel-width", po::value<std::int16_t>());
-    desc.add_options()("active-pixel-height", po::value<std::int16_t>());
-    desc.add_options()("pixel-array-width", po::value<std::int16_t>());
-    desc.add_options()("pixel-array-height", po::value<std::int16_t>());
-    desc.add_options()("sensor-timing", po::value<std::int32_t>());
-    desc.add_options()("output-timing", po::value<std::int32_t>());
-    desc.add_options()("fps", po::value<float>());
-    desc.add_options()("input-file,i", po::value<std::string>());
-    desc.add_options()("output-file,o", po::value<std::string>());
+    const po::options_description desc = [] {
+        po::options_description desc("Options");
+        desc.add_options()("active-pixel-width", po::value<std::int16_t>());
+        desc.add_options()("active-pixel-height", po::value<std::int16_t>());
+        desc.add_options()("pixel-array-width", po::value<std::int16_t>());
+        desc.add_options()("pixel-array-height", po::value<std::int16_t>());
+        desc.add_options()("sensor-timing", po::value<std::int32_t>());
+        desc.add_options()("output-timing", po::value<std::int32_t>());
+        desc.add_options()("fps", po::value<float>());
+        desc.add_options()("input-file,i", po::value<std::string>());
+        desc.add_options()("output-file,o", po::value<std::string>());
+        return desc;
+    }();
     
-    po::positional_options_description pdesc;
-    pdesc.add("input-file", 1);
+    const po::positional_options_description pdesc = [] {
+        po::positional_options_description pdesc;
+        pdesc.add("input-file", 1);
+        return pdesc;
+    }();
 
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(desc).positional(pdesc).run(), vm);
-    po::notify(vm);
+    const po::variables_map vm = [&] {
+        po::variables_map vm;
+        po::store(po::command_line_parser(argc, argv).options(desc).positional(pdesc).run(), vm);
+        po::notify(vm);
+        return vm;
+    }();
 
     const auto input_filename = [&](const std::string& key) {
         if (vm.count(key)) {
@@ -85,39 +94,48 @@ auto main(const int argc, const char * const * const argv) -> int
         }
     }("input-file");
 
-    std::ifstream input_file(input_filename);
-    input_file.exceptions(std::ios::failbit | std::ios::badbit);
-    
-    input_file.seekg(conf_offset);
-    configuration conf;
-    input_file.read(reinterpret_cast<std::fstream::char_type*>(&conf), sizeof(conf));
+    const configuration conf = [&] {
+        std::ifstream input_file(input_filename);
+        input_file.exceptions(std::ios::failbit | std::ios::badbit);
+
+        input_file.seekg(conf_offset);
+        configuration conf;
+        input_file.read(reinterpret_cast<std::fstream::char_type*>(&conf), sizeof(conf));
+        return conf;
+    }();
 
     std::cerr << input_filename << ": " << conf << std::endl;
     
     if (vm.count("output-file")) {
-        cond_set_field<std::int16_t>(conf.active_pixel_width, vm, "active-pixel-width");
-        cond_set_field<std::int16_t>(conf.active_pixel_height, vm, "active-pixel-height");
-        cond_set_field<std::int16_t>(conf.pixel_array_width, vm, "pixel-array-width");
-        cond_set_field<std::int16_t>(conf.pixel_array_height, vm, "pixel-array-height");
-        cond_set_field<std::int32_t>(conf.sensor_timing, vm, "sensor-timing");
-        cond_set_field<std::int32_t>(conf.output_timing, vm, "output-timing");
-        cond_set_field<float>(conf.fps, vm, "fps");
+        const configuration new_conf = [&] {
+            configuration new_conf = conf;
+            cond_set_field<std::int16_t>(new_conf.active_pixel_width, vm, "active-pixel-width");
+            cond_set_field<std::int16_t>(new_conf.active_pixel_height, vm, "active-pixel-height");
+            cond_set_field<std::int16_t>(new_conf.pixel_array_width, vm, "pixel-array-width");
+            cond_set_field<std::int16_t>(new_conf.pixel_array_height, vm, "pixel-array-height");
+            cond_set_field<std::int32_t>(new_conf.sensor_timing, vm, "sensor-timing");
+            cond_set_field<std::int32_t>(new_conf.output_timing, vm, "output-timing");
+            cond_set_field<float>(new_conf.fps, vm, "fps");
+            return new_conf;
+        }();
 
         const auto output_filename = vm["output-file"].as<std::string>();
         std::ofstream output_file(output_filename);
         output_file.exceptions(std::ios::failbit | std::ios::badbit);
 
-        input_file.seekg(0);
-        std::copy(std::istreambuf_iterator<char>(input_file), std::istreambuf_iterator<char>(), std::ostreambuf_iterator<char>(output_file));
+        [&] {
+            std::ifstream input_file(input_filename);
+            std::copy(std::istreambuf_iterator<char>(input_file), std::istreambuf_iterator<char>(), std::ostreambuf_iterator<char>(output_file));
+        }();
 
         // The active pixel width and height as 32-bit integer @ wh32_offset needs to be written as well.
         output_file.seekp(wh32_offset);
-        little_int32_t active_pixel_width = static_cast<std::int32_t>(conf.active_pixel_width);
-        output_file.write(reinterpret_cast<std::fstream::char_type*>(&active_pixel_width), sizeof(active_pixel_width));
-        little_int32_t active_pixel_height = static_cast<std::int32_t>(conf.active_pixel_height);
-        output_file.write(reinterpret_cast<std::fstream::char_type*>(&active_pixel_height), sizeof(active_pixel_height));
+        const little_int32_t active_pixel_width = static_cast<std::int32_t>(new_conf.active_pixel_width);
+        output_file.write(reinterpret_cast<const std::fstream::char_type*>(&active_pixel_width), sizeof(active_pixel_width));
+        const little_int32_t active_pixel_height = static_cast<std::int32_t>(new_conf.active_pixel_height);
+        output_file.write(reinterpret_cast<const std::fstream::char_type*>(&active_pixel_height), sizeof(active_pixel_height));
         output_file.seekp(conf_offset);
-        output_file.write(reinterpret_cast<std::fstream::char_type*>(&conf), sizeof(conf));
+        output_file.write(reinterpret_cast<const std::fstream::char_type*>(&new_conf), sizeof(new_conf));
 
         std::cerr << output_filename << ": " << conf << std::endl;
     }
